dcf1689764338f79b085350c096359ef92a7b14f
Bricegnanago <gnanagobrice@gmail.com>
2020-06-26 04:23:22 +0200

    projet de marie : ajout de plugin de type select

--- js/script.js
+++ js/script.js
@@ -11,5 +11,14 @@ $(document).ready(function () {
         $("#personnal").show(1000);
         
         $("#next").show(1000);
-    })
+    });
+    
+    $( "#formId" ).delegate( "*", "focus blur", function() {
+        var elem = $( this );
+        setTimeout(function() {
+          elem.toggleClass( "shadow", elem.is( ":focus" ) );
+        }, 0 );
+      });
+    // alert($("#formId input[type='text'], #formId input[type='file'], #formId select").length);
+
   });
